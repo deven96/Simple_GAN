ab88907812d9170ff5dbd06bb33a4082963f3bed
Deven96 <diretnandomnan@gmail.com>
2018-12-21 06:55:20 +0100

    trying git token

--- docs.sh
+++ docs.sh
@@ -4,7 +4,10 @@ cd ./docs
 sphinx-apidoc -f -o source/ ../adversarials
 make html
 cd ./build/html
+git config --global push.default simple
+git config --global user.email "travis@travis-ci.com"
+git config --global user.name "Travis CI"
 git add .
 git commit -m "rebuilt docs"
 git checkout -b gh-pages
-git push origin gh-pages
+git push -q https://${GITHUB_TOKEN}@github.com/deven96/Simple_GAN.git gh-pages
