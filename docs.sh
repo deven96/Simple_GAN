c7dce062ea395c5c0755917ab0caac7f2215cba0
Deven96 <diretnandomnan@gmail.com>
2018-12-21 07:48:12 +0100

    filter branch

--- docs.sh
+++ docs.sh
@@ -4,11 +4,13 @@ git clone https://${GITHUB_TOKEN}@github.com/deven96/Simple_GAN.git
 cd ./Simple_GAN/docs
 sphinx-apidoc -f -o source/ ../adversarials
 make html
-cd ./build/html
 git config --global push.default simple
 git config --global user.email "travis@travis-ci.com"
 git config --global user.name "Travis CI"
+git filter-branch --prune-empty --subdirectory-filter build/html master 
 git add .
 git commit -m "rebuilt docs"
+git stash
 git checkout gh-pages
+git stash apply
 git push -q https://${GITHUB_TOKEN}@github.com/deven96/Simple_GAN.git gh-pages --force
