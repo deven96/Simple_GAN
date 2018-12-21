408e666e4154e78486167cb4e360da7903c66715
Deven96 <diretnandomnan@gmail.com>
2018-12-21 09:22:29 +0100

    using env variable

--- docs.sh
+++ docs.sh
@@ -12,7 +12,7 @@ git checkout -b gh-pages
 shopt -s extglob
 rm -r ./!(docs)/
 #copy contents of html to root
-cp -R $TRAVIS_BUILD_DIR/docs/html $TRAVIS_BUILD_DIR/
+cp -R ${TRAVIS_BUILD_DIR}/docs/html ${TRAVIS_BUILD_DIR}/
 #remove html and accompanying docs  
 rm -r ./docs
 git commit -am "rebuilt docs"
