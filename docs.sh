b5143f4d22a991c7de6b0857476839be06e1a712
Deven96 <diretnandomnan@gmail.com>
2018-12-21 06:32:04 +0100

    added executable

--- docs.sh
+++ docs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 cd ./docs
-sphinx-apidoc -f source/ ../adversarials
+sphinx-apidoc -f -o source/ ../adversarials
 make html
 cd ./build/html
 git add .
 git commit -m "rebuilt docs"
+git fetch --unshallow
 git push origin gh-pages
