9b6ac50a0433441a6118ad3040bfd13d6c0a0b78
Deven96 <diretnandomnan@gmail.com>
2018-12-21 08:30:16 +0100

    attempt rebuild

--- docs.sh
+++ docs.sh
@@ -6,13 +6,13 @@ sphinx-apidoc -f -o source/ ../adversarials
 make html
 cd ..
 mkdir ../temp
-cp -rf ./docs/html/* ../temp/
+cp -Rf ./docs/html/. ../temp
 git config --global push.default simple
 git config --global user.email "travis@travis-ci.com"
 git config --global user.name "Travis CI"
 git checkout gh-pages
 #remove existing files
 rm -R ./*
-cp -rf ../temp/* .
+cp -R ../temp/* .
 git commit -am "rebuilt docs"
 git push -q https://${GITHUB_TOKEN}@github.com/deven96/Simple_GAN.git gh-pages --force
