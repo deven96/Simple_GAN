6b83b66edff04d3da28288eccc5f020342cd7929
Deven96 <diretnandomnan@gmail.com>
2018-12-21 08:55:44 +0100

    attempt push

--- docs.sh
+++ docs.sh
@@ -8,10 +8,12 @@ git config --global push.default simple
 git config --global user.email "travis@travis-ci.com"
 git config --global user.name "Travis CI"
 git checkout -b gh-pages
-#remove existing files
+#remove existing files except html
 shopt -s extglob
-rm -r ./!(docs/html)/
-cp -R ./docs/html/* .
+rm -r ./!(docs)/
+#copy contents of html to root
+cp -R ./docs/html/ .
+#remove html and accompanying docs  
 rm -r ./docs
 git commit -am "rebuilt docs"
 git push -q https://${GITHUB_TOKEN}@github.com/deven96/Simple_GAN.git gh-pages --force
