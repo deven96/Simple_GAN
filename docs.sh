afe0489b3ab45d01efacf5a69ed475d529604c4e
Deven96 <diretnandomnan@gmail.com>
2018-12-21 07:00:30 +0100

    trying git token

--- docs.sh
+++ docs.sh
@@ -10,5 +10,5 @@ git config --global user.name "Travis CI"
 git add .
 git commit -m "rebuilt docs"
 git checkout -b gh-pages
-git pull -q https://${GITHUB_TOKEN}@github.com/deven96/Simple_GAN.git
+git pull -q https://${GITHUB_TOKEN}@github.com/deven96/Simple_GAN.git gh-pages
 git push -q https://${GITHUB_TOKEN}@github.com/deven96/Simple_GAN.git gh-pages
