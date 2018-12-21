2c048e3c7be004156559b1e15460f5cc6c296e28
Deven96 <diretnandomnan@gmail.com>
2018-12-21 07:49:20 +0100

    filter branch

--- docs.sh
+++ docs.sh
@@ -4,6 +4,7 @@ git clone https://${GITHUB_TOKEN}@github.com/deven96/Simple_GAN.git
 cd ./Simple_GAN/docs
 sphinx-apidoc -f -o source/ ../adversarials
 make html
+cd ..
 git config --global push.default simple
 git config --global user.email "travis@travis-ci.com"
 git config --global user.name "Travis CI"
