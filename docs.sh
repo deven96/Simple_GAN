3ae4795daeeb1a545c2f6d3ba7d0f5f5d093e2d3
Deven96 <diretnandomnan@gmail.com>
2018-12-21 07:51:15 +0100

    filter branch

--- docs.sh
+++ docs.sh
@@ -8,10 +8,7 @@ cd ..
 git config --global push.default simple
 git config --global user.email "travis@travis-ci.com"
 git config --global user.name "Travis CI"
-git filter-branch --prune-empty --subdirectory-filter build/html master 
+git filter-branch --prune-empty --subdirectory-filter build/html gh-pages 
 git add .
 git commit -m "rebuilt docs"
-git stash
-git checkout gh-pages
-git stash apply
 git push -q https://${GITHUB_TOKEN}@github.com/deven96/Simple_GAN.git gh-pages --force
