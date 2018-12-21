6c72ea11cd854312efd8f3825c0a7f1b4384dc2b
Deven96 <diretnandomnan@gmail.com>
2018-12-21 07:14:03 +0100

    trying stash 2.0

--- docs.sh
+++ docs.sh
@@ -12,4 +12,5 @@ git commit -m "rebuilt docs"
 git checkout -b gh-pages
 git stash
 git pull -q https://${GITHUB_TOKEN}@github.com/deven96/Simple_GAN.git gh-pages
+git stash apply
 git push -q https://${GITHUB_TOKEN}@github.com/deven96/Simple_GAN.git gh-pages
